1b8c8cd940206d7c93b7d6a166bdbd0a7c58b2cc
pangtong <pangtong@98kplay.com>
2019-06-28 15:39:27 +0800

    截图文件夹可以清理

--- lib/viewmodel/setting_viewmodel.dart
+++ lib/viewmodel/setting_viewmodel.dart
@@ -11,27 +11,45 @@ class SettingViewModel extends ViewModel {
 
   void clearCache() async {
     streamAdd(isLoading, true);
-    final directory = Directory(await DefaultCacheManager().getFilePath());
+    final cacheDir = Directory(await DefaultCacheManager().getFilePath());
+    final documentDir =
+        Directory((await getApplicationDocumentsDirectory()).path);
 
-    if (directory.existsSync()) {
-      directory.listSync().forEach((v) => v.deleteSync());
-
-      await calculateSize();
+    if (cacheDir.existsSync()) {
+      cacheDir.listSync().forEach((v) => v.deleteSync());
     }
+    if (documentDir.existsSync()) {
+      documentDir
+          .listSync()
+          .where((v) => v.path.contains(".png"))
+          .forEach((v) => v.deleteSync());
+    }
+
+    await calculateSize();
 
     streamAdd(isLoading, false);
   }
 
   Future<Null> calculateSize() async {
-    final directory = Directory(await DefaultCacheManager().getFilePath());
+    final cacheDir = Directory(await DefaultCacheManager().getFilePath());
+    final documentDir =
+        Directory((await getApplicationDocumentsDirectory()).path);
 
     int size = 0;
-    if (directory.existsSync()) {
-      directory
+    if (cacheDir.existsSync()) {
+      cacheDir
           .listSync()
           .map((v) => File(v.path).lengthSync())
           .forEach((length) => size += length);
     }
+    if (documentDir.existsSync()) {
+      documentDir
+          .listSync()
+          .where((v) => v.path.contains(".png"))
+          .map((v) => File(v.path).lengthSync())
+          .forEach((length) => size += length);
+    }
+
     streamAdd(cacheSize, ByteUtil.calculateSize(size));
   }
 
