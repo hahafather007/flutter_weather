740cb48cdece0c80f98347a8fc151612a36e1adb
pangtong <pangtong@98kplay.com>
2019-08-15 16:32:18 +0800

    开屏画面优化

--- lib/view/page/splash_page.dart
+++ lib/view/page/splash_page.dart
@@ -1,20 +1,32 @@
 import 'package:flutter_weather/commom_import.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SplashPage extends StatefulWidget {
   @override
   State createState() => SplashState();
 }
 
-class SplashState extends State<SplashPage> {
+class SplashState extends State<SplashPage> with StreamSubController {
   @override
   void initState() {
     super.initState();
 
-    SharedDepository().initShared().then((_) {
-      EventSendHolder()
-          .sendEvent(tag: "themeChange", event: SharedDepository().themeColor);
+    bindSub(Observable.zip2(
+            Stream.fromFuture(Future.delayed(const Duration(seconds: 1))),
+            Stream.fromFuture(SharedDepository().initShared()),
+            (a, b) => true)
+        .map((_) => SharedDepository().themeColor)
+        .doOnData((color) {
+      EventSendHolder().sendEvent(tag: "themeChange", event: color);
       push(context, page: HomePage(), replace: true);
-    });
+    }).listen(null));
+  }
+
+  @override
+  void dispose() {
+    subDispose();
+
+    super.dispose();
   }
 
   @override
@@ -24,6 +36,8 @@ class SplashState extends State<SplashPage> {
         body: Image.asset(
           "images/splash.png",
           fit: isAndroid ? BoxFit.fill : BoxFit.fitHeight,
+          height: getScreenHeight(context),
+          width: getScreenWidth(context),
         ),
       ),
       onWillPop: () async => false,
